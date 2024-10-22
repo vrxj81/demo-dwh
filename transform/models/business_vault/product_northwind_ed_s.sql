1add54724bbc3a107dfe146ea50032698dc62feb
Johan Vrolix <johan@vrolix.eu>
2024-10-22 16:59:36 +0200

    End date views on satellites

---
+++ transform/models/business_vault/product_northwind_ed_s.sql
@@ -0,0 +1,21 @@
+{%- set yaml_metadata -%}
+sat_v0: product_northwind_s
+hashkey: hk_product_h
+hashdiff: hd_product_s
+add_is_current_flag: true
+{%- endset -%}
+
+{%- set metadata_dict = fromyaml(yaml_metadata) %}
+{%- set sat_v0 = metadata_dict.get('sat_v0') %}
+{%- set hashkey = metadata_dict.get('hashkey') %}
+{%- set hashdiff = metadata_dict.get('hashdiff') %}
+{%- set add_is_current_flag = metadata_dict.get('add_is_current_flag') %}
+
+{{
+  datavault4dbt.sat_v1(
+    sat_v0=sat_v0,
+    hashkey=hashkey,
+    hashdiff=hashdiff,
+    add_is_current_flag=add_is_current_flag
+  )
+}}
